8a42a0f525cb927e068392052b569778e9cdf9b6
heisluft <heisluftlp@gmail.com>
2022-04-14 18:03:20 +0200

    fully document common and bootstrap source sets

--- src/bootstrap/java/de/heisluft/launcher/bootstrap/ProxyingFormatter.java
+++ src/bootstrap/java/de/heisluft/launcher/bootstrap/ProxyingFormatter.java
@@ -7,6 +7,10 @@ import org.apache.logging.log4j.Logger;
 import java.util.logging.Formatter;
 import java.util.logging.LogRecord;
 
+/**
+ * This is a Formatter redirecting all LogRecords to Log4j, returning an empty string so jul does
+ * not print its log messages by itself
+ */
 public class ProxyingFormatter extends Formatter {
 
   @Override
--- src/common/java/de/heisluft/launcher/common/Util.java
+++ src/common/java/de/heisluft/launcher/common/Util.java
@@ -6,7 +6,31 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+/**
+ * A shared utility class
+ */
 public class Util {
+
+  /**
+   * Util should not be instantiated
+   */
+  private Util() {throw new UnsupportedOperationException();}
+
+  /**
+   * Copies all bytes from is to os, using a buffer size of bufSize. It closes all streams
+   * afterwards. If one stream is null, this method closes the other stream if nonnull, returning
+   * without reading or writing
+   *
+   * @param is
+   *     the inputStream to be read from
+   * @param os
+   *     the outputStream to be written to
+   * @param bufSize
+   *     the buffer size to be used
+   *
+   * @throws IOException
+   *     if a reading, writing or closing operation fails
+   */
   public static void copyStream(InputStream is, OutputStream os, int bufSize) throws IOException {
     if(is == null || os == null) {
       if(os != null) os.close();
@@ -21,8 +45,17 @@ public class Util {
     is.close();
     os.close();
   }
+
+  /**
+   * Converts a JUL Level to its Corresponding Log4j level
+   *
+   * @param level
+   *     the JUL Level
+   *
+   * @return the corresponding Log4j level
+   */
   public static Level jul2log4j(java.util.logging.Level level) {
-    if(level == java.util.logging.Level.SEVERE)return Level.ERROR;
+    if(level == java.util.logging.Level.SEVERE) return Level.ERROR;
     if(level == java.util.logging.Level.WARNING) return Level.WARN;
     if(level == java.util.logging.Level.INFO) return Level.INFO;
     if(level == java.util.logging.Level.CONFIG) return Level.INFO;
